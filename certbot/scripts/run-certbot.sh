aa858c76ef6f91368bd57a885e5a4b0f930ba2fa
Austin Gil <austin@stegosource.com>
2018-09-12 15:21:13 -0700

    Conditionals for certbot

--- certbot/scripts/run-certbot.sh
+++ certbot/scripts/run-certbot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Run certbot to get cert
-# Add --staging for testing purposes
-# Add --force-renewal to force a renew
-certbot certonly --webroot -w /data/letsencrypt --agree-tos --non-interactive --text -d "$DOMAIN" --email "$EMAIL"
+if [ $ENVIRONMENT -ne "development" ]; then
+	# Run certbot to get cert
+	# Add --staging for testing purposes
+	# Add --force-renewal to force a renew
+	certbot certonly --webroot -w /data/letsencrypt --agree-tos --non-interactive --text -d "$DOMAIN" --email "$EMAIL"
 
-# Create directory with domain name within shared volume
-mkdir -p /data/certs/"$DOMAIN"
-# Move cert files to shared volume at folder name
-cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem,privkey.pem,chain.pem,fullchain.pem} "$_"
+	# Create directory with domain name within shared volume
+	mkdir -p /data/certs/"$DOMAIN"
+	# Move cert files to shared volume at folder name
+	cp /etc/letsencrypt/live/"$DOMAIN"/{cert.pem,privkey.pem,chain.pem,fullchain.pem} "$_"
+else echo "No certs for dev stuff"
+fi
